6450d198e3de947227f399b45914d0bdba0b5f0b
Luca Errani <luca.errani1@gmail.com>
2020-04-03 16:57:37 +0200

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestPropertyGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MyObject Container { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            // Container creation
+            Container = new MyObject();
+
+            // Add Circular dependending elements
+            Container.Elements = new List<SubObj> { new SubObj() { Parent = Container }, new SubObj() { Parent = Container } };
+        }
+    }
+}
---
+++ MyObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPropertyGrid
+{
+    public class MyObject
+    {
+        public List<SubObj> Elements { get; set; }
+    }
+
+    public class SubObj
+    {
+        public int Value { get; set; }
+        public MyObject Parent { get; set; }
+    }
+}
